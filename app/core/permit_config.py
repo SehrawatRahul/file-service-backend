82eb03dc3687da8283eb27c3ed95ee21dc3e004f
Harshvardhan <harshvardhan1@veersatech.com>
2025-11-14 14:53:36 +0530

    fix env variables

--- app/core/permit_config.py
+++ app/core/permit_config.py
@@ -1,8 +1,12 @@
+import os
 from permit import Permit
+from dotenv import load_dotenv
+
+load_dotenv()
 
 permit = Permit(  # in production, you might need to change this url to fit your deployment
-    pdp="https://cloudpdp.api.permit.io",  # your api key
-    token="permit_key_A7fcSuEtekjPP8wqrBbCllnvZ5eIm0xqJ4y550SC4cAgivpqhcGWttS8Sr92SMjqKSr5NfP8EU4JXf6FnMBA72",
+    pdp=os.getenv('PDP_URL'),  # your api key
+    token=os.getenv('PERMIT_TOKEN'),
 )
 
 user = {
